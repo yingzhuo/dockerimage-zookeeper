0296fb1569d78cdc562d40d1bb2ce87d86ce837a
yingzhuo <yingzhor@gmail.com>
2019-11-26 11:30:44 +0800

    2019-11-26 11:30:44

--- 3.5.6/docker-entrypoint.sh
+++ 3.5.6/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p /opt/zookeeper/data
 
-cat -n "${ZOOKEEPER_MYID}" > /opt/zookeeper/data/myid
+echo -n "${ZOOKEEPER_MYID}" > /opt/zookeeper/data/myid
 
 bash /opt/zookeeper/bin/zkServer.sh start
 
